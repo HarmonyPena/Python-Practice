cfe92c5d274b6a6b5579dce424fcace97b9f55c2
Harmony Jackson <Jackson.Harmony@outlook.com>
2021-10-16 20:03:30 -0500

    Initial commit

---
+++ Practice.py
@@ -0,0 +1,7 @@
+menu = 'Cinnamon, Vanilla, and Pumpkin'
+
+print('Hello and welcome!')
+
+selection = input('What kind of cappuccino can I get you today? \nWe have ' + menu +': ')
+
+print('Awesome! we will that ' + selection + ' capucinno ready in about 5 minutes.')
