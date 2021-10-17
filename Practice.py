96033447b786a570d53c51cb63f2bef950be608d
HarmonyJackson <92655597+HarmonyJackson@users.noreply.github.com>
2021-10-16 20:09:02 -0500

    Update Practice.py

--- Practice.py
+++ Practice.py
@@ -5,3 +5,6 @@ print('Hello and welcome!')
 selection = input('What kind of cappuccino can I get you today? \nWe have ' + menu +': ')
 
 print('Awesome! we will that ' + selection + ' capucinno ready in about 5 minutes.')
+
+
+print('test')
